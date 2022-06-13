e7757469e5c78be40d0bba3e24ae3a4822fb457b
omegabiebel <superbiebel@gmail.com>
2022-06-13 22:24:50 +0200

    updated gitignore

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,22 @@
+use std::collections::HashMap;
+use uuid::Uuid;
 
+struct EntityRegistry {
+    map: HashMap<Uuid, HashMap<String, PlanningValue>> //entityUUID, PlanningVariableName, planning value
+}
+
+enum PlanningValue {
+    String(String),
+    Int(i32),
+    Double(f64),
+    Id(Uuid, String) //references other entity through the lookup system (entityUUID, String)
+}
 
 #[cfg(test)]
 mod tests {
+
     #[test]
     fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+
     }
 }
